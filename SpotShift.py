98b4f0185449c103263e853e6d751ccd73bfec7d
rye <rvyeinq@gmail.com>
2023-03-01 08:00:14 -0600

    Update SpotShift.py

--- SpotShift.py
+++ SpotShift.py
@@ -14,14 +14,14 @@
 #
 # https://github.com/rvye/spotshift
 
-
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from ytmusicapi import YTMusic
 import requests as req
-import random as rdm
 import spotipy as sp
 import csv
+import sys
 import os
 import re
 from dotenv import load_dotenv
@@ -35,51 +35,89 @@ CLIENT_ID = "d92022b036d5409198689c7719bd123c"
 CLIENT_SECRET = "3a85d8d5b7a14e6383935de817a21043"
 
 client_credentials_manager = SpotifyClientCredentials(
-    client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+  client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
 session = sp.Spotify(client_credentials_manager=client_credentials_manager)
 
+print(
+  " ___           _   ___ _    _  __ _\n/ __|_ __  ___| |_/ __| |_ (_)/ _| |_\n\\__ \ '_ \/ _ \\  _\\__ \\ ' \| |  _|  _|\n|___/ .__/\\___/\\__|___/_||_|_|_|  \__|     \n    |_|                               "
+)
+
+def dev():
+  music = ytmusic()
+
+  
+sshft = int(
+  input(
+    "Select a source to transfer from: \n[1] Spotify to Apple Music\n[2] Spotify to Youtube Music\n[3] Apple Music to Spotify\n[4] Apple Music to YouTube Music\n[5] YouTube Music to Spotify[6] YouTube Music to Apple Music\n[0] Quit\n\n"
+  ))
 
 
-print(" ___           _   ___ _    _  __ _\n/ __|_ __  ___| |_/ __| |_ (_)/ _| |_\n\\__ \ '_ \/ _ \\  _\\__ \\ ' \| |  _|  _|\n|___/ .__/\\___/\\__|___/_||_|_|_|  \__|     \n    |_|                               ")
+def transferToSpotifyAAPL(url):
+  print("AAPL")
 
 
-sshft = int(input("Select a source to transfer from: \n[1] Spotify to Apple Music\n[2] Apple Music to Spotify\n\n[0] Quit\n\n"))
+def transferToSpotifyGOOG(url):
+  print(url)
 
-def transferToSpotify(url):
-    print("AAPL")
-def transferToAAPL(url):
-    # Reading from playlist
-    if match := re.match(r"https://open.spotify.com/playlist/(.*)\?", url):
-        playlist_uri = match.groups()[0]
-    else:
-        raise ValueError("Expected format: https://open.spotify.com/playlist/...")
 
-    tracks = session.playlist_tracks(playlist_uri)["items"]
+def transferToGOOGaapl():
+  print("GA")
 
-    with open("a.csv", "w", encoding="utf-8") as file:
-        writer = csv.writer(file)
 
-        # write header column names
-        writer.writerow(["track", "artist"])
+def transferToGOOGspotify(url):
+  # Reading from playlist
+  if match := re.match(r"https://open.spotify.com/playlist/(.*)\?", url):
+    playlist_uri = match.groups()[0]
+  else:
+    raise ValueError("Expected format: https://open.spotify.com/playlist/...")
 
-        # extract name and artist
-        for track in tracks:
-            name = track["track"]["name"]
-            artists = ", ".join(
-                [artist["name"] for artist in track["track"]["artists"]]
-            )
+  tracks = session.playlist_tracks(playlist_uri)["items"]
 
-            # write to csv
-            writer.writerow([name, artists])
-            print([name, artists])
+  with open("a.csv", "w", encoding="utf-8") as file:
+    writer = csv.writer(file)
 
-    # Transferring to apple music
+    # write header column names
+    writer.writerow(["track", "artist"])
+
+    # extract name and artist
+    for track in tracks:
+      name = track["track"]["name"]
+      artists = ", ".join(
+        [artist["name"] for artist in track["track"]["artists"]])
+
+      # write to csv
+      writer.writerow([name, artists])
+      print([name, artists])
+
+
+  # req.post()
+  
+def transferToAAPLgoog(url):
+  print(url)
+
+
+def transferToAAPLspot(url):
+  print(url)
+
+
 if sshft == 1:
-    transferToAAPL(input("Paste your playlist link here: "))
+  # transferToAAPL(input("Paste your playlist link here: "))
+  transferToAAPLspot("a")
 elif sshft == 2:
-    transferToSpotify("aapl")
+  #transferToGOOGspotify(input("Paste your playlist link here: "))
+  print("a")
+
+  dev()
+elif sshft == 3:
+  transferToSpotifyAAPL("apple to spotify")
+elif sshft == 4:
+  transferToGOOGaapl("apple to google")
+elif sshft == 5:
+  transferToSpotifyGOOG("yt to spot")
+elif sshft == 6:
+  transferToAAPLgoog("youtube to apple")
 elif sshft == 0:
-    sys.exit()
+  sys.exit()
 else:
-    print("Enter a valid number.")
+  print("Enter a valid number.")
