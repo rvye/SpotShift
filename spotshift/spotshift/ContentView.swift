424397062736fdba753b7b739bc5406be62369bb
kay <k@k.com>
2023-03-05 16:40:18 -0600

    add files

---
+++ spotshift/spotshift/ContentView.swift
@@ -0,0 +1,27 @@
+//  ContentView.swift
+//  spotshift
+//
+//  Copyright (c) 2023 rvye
+//  MIT License
+//
+//  Created on 3/5/23
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spotshift/spotshift/spotshiftApp.swift
@@ -0,0 +1,18 @@
+//  ContentView.swift
+//  spotshift
+//
+//  Copyright (c) 2023 rvye
+//  MIT License
+//
+//  Created on 3/5/23
+
+import SwiftUI
+
+@main
+struct spotshiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
